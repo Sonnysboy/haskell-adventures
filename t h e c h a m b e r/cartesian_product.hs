ce1ae88540882b4aae1bbb9bfdbdda09cbc5c947
M K Messmer <sonnysboy4@gmail.com>
2024-01-23 09:11:50 -0800

    holy

--- t h e c h a m b e r/cartesian_product.hs
+++ t h e c h a m b e r/cartesian_product.hs
@@ -2,6 +2,5 @@
 
 cartesianProduct :: [a] -> [a] -> [(a,a)]
 cartesianProduct x y
-    | length x /= length y = error "Lengths must be equal"
-    | otherwise = [(a, b) | a <- x, b <- y]
+     = [(a, b) | a <- x, b <- y]
 
