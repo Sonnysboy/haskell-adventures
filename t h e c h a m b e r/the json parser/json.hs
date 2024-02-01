35119267cd2dad8167733a67f8342c62f54d0ce5
M K Messmer <sonnysboy4@gmail.com>
2024-02-01 11:40:40 -0800

    alright here's this. we might want to start on the parser now :(

--- t h e c h a m b e r/the json parser/json.hs
+++ t h e c h a m b e r/the json parser/json.hs
@@ -1,50 +1,78 @@
+import Control.Applicative
 import Data.Map (Map)
+import Data.Map qualified
+
 newtype JsonError = JsonError String
-    deriving Show
+  deriving (Show)
 
-data JsonValue = JsonNull
-               | JsonBool Bool
-               | JsonNumber Double
-               | JsonString String
-               | JsonArray [JsonValue]
-               | JsonObject (Map String JsonValue)
-               deriving (Show, Eq)
+data JsonValue
+  = JsonNull
+  | JsonBool Bool
+  | JsonNumber Double
+  | JsonString String
+  | JsonArray [JsonValue]
+  | JsonObject (Map String JsonValue)
+  deriving (Show, Eq)
 
+{-
+
+Converts a map of ToJsonables into a JsonObject
+
+-}
+fromMap :: (ToJson a) => Map String a -> JsonValue
+fromMap map = JsonObject $ Data.Map.map toJson map
+
+{--
+    Converts a JsonObject to a map
+-}
+toMap :: (FromJson a) => JsonValue -> Map String a
+toMap (JsonObject map) = snd $ Data.Map.mapEither fromJson map
 
 instance Semigroup JsonValue where
-    (<>) :: JsonValue -> JsonValue -> JsonValue
-    (<>) (JsonObject map1) (JsonObject map2) = JsonObject $ map1 <> map2
-    (<>) (JsonArray a) (JsonArray b)         = JsonArray  $ a    <> b
-    (<>) _ _                                 = JsonNull
+  (<>) :: JsonValue -> JsonValue -> JsonValue
+  (<>) (JsonObject map1) (JsonObject map2) = JsonObject $ map1 <> map2
+  (<>) (JsonArray a) (JsonArray b) = JsonArray $ a <> b
+  (<>) _ _ = JsonNull
+
+instance Monoid JsonValue where
+  mempty = JsonNull
 
 class ToJson a where
-    toJson :: a -> JsonValue
+  toJson :: a -> JsonValue
 
 instance ToJson Double where
-    toJson = JsonNumber
+  toJson = JsonNumber
 
 instance ToJson Bool where
-    toJson = JsonBool
-
-instance ToJson String where
-    toJson = JsonString
+  toJson = JsonBool
 
 instance (ToJson a) => ToJson [a] where
-    toJson xs = JsonArray $ toJson <$> xs
+  toJson xs = JsonArray $ toJson <$> xs
 
+instance {-# OVERLAPPING #-} ToJson String where
+  toJson = JsonString
 
 class FromJson a where
-    fromJson :: JsonValue -> Either JsonError a
+  fromJson :: JsonValue -> Either JsonError a
 
 instance FromJson Double where
-    fromJson (JsonNumber num) = Right num
-    fromJson _                = Left $ JsonError "can only read number"
+  fromJson (JsonNumber num) = Right num
+  fromJson _ = Left $ JsonError "only numbers"
+
 instance FromJson String where
-    fromJson (JsonString x) = Right x
-    fromJson _ = Left $ JsonError "can only read string"
+  fromJson (JsonString x) = Right x
+  fromJson _ = Left $ JsonError "only strings"
+
 instance FromJson Bool where
-    fromJson (JsonBool x) = Right x
-    fromJson _ = Left $ JsonError "can only read string"
+  fromJson (JsonBool x) = Right x
+  fromJson _ = Left $ JsonError "only bools"
+
+instance (FromJson a) => FromJson (Map String a) where
+  fromJson (JsonObject m)
+    | null $ fst value = Right $ snd value
+    | otherwise = Left $ JsonError ("something went wrong while parsing: " ++ foldr (\(JsonError msg) acc -> (acc ++ msg) ++ "\n") "" (fst value))
+    where
+      value = Data.Map.mapEither fromJson m
 
 instance (FromJson a) => FromJson [a] where
-    fromJson (JsonArray arr) = mapM fromJson arr
+  fromJson (JsonArray arr) = mapM fromJson arr
