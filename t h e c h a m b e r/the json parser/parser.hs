15060bec139014da979a2f23ebdc24fe3a2ce389
M K Messmer <sonnysboy4@gmail.com>
2024-02-02 16:17:50 -0800

    applicative

--- t h e c h a m b e r/the json parser/parser.hs
+++ t h e c h a m b e r/the json parser/parser.hs
@@ -18,6 +18,22 @@ instance Functor Parser where
     let result = parse p x
     in first transform <$> result
 
+
+instance Applicative Parser where
+  pure a = Parser $ \x -> Just (a, x) -- questionable
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  Parser f <*> Parser g = Parser $ \x -> case f x of -- ok so we're parsing using f
+    Nothing -> Nothing
+    -- (a -> b, string)
+    Just (r, s') ->  case g s' of -- Maybe (b, String)
+        Nothing -> Nothing
+        -- (a, String) -> (Just (apply a function (a -> b) to type a so now it's a b), String) 
+        Just (r', s'') -> Just (r r', s'')
+
+
+
+
+
 parseChar :: Parser Char
 parseChar = Parser $ \x -> parseChar' x
     where parseChar' "" = Nothing
