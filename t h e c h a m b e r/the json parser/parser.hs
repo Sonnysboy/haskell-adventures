be1cb494a24f653205be92bf9b9cb298099788d0
M K Messmer <sonnysboy4@gmail.com>
2024-02-05 09:28:13 -0800

    it works!!

--- t h e c h a m b e r/the json parser/parser.hs
+++ t h e c h a m b e r/the json parser/parser.hs
@@ -1,5 +1,6 @@
 -- this is gonna be hard
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Use tuple-section" #-}
 
 import Control.Applicative
@@ -7,8 +8,8 @@ import Control.Arrow
 import Control.Monad
 import Data.Char
 import Data.Foldable
-import Json
 import Data.Map
+import Json
 
 newtype Parser a = Parser (String -> Maybe (a, String))
 
@@ -108,8 +109,7 @@ string = between '"' '"'
 --   Nothing -> error $ "x: " ++ x
 
 object :: Parser JsonValue
-object = Json.fromPairs <$> (eatWhitespace *> char '{' >> parseInternals <* char '}' <* eatWhitespace)
-
+object = Json.fromPairs <$> (eatWhitespace >> char '{' >> parseInternals <* eatWhitespace <* char '}')
 
 -- chain :: Parser String -> Parser b -> Parser b
 -- chain (Parser f) (Parser g) = Parser $ \x -> case f x of
@@ -117,9 +117,13 @@ object = Json.fromPairs <$> (eatWhitespace *> char '{' >> parseInternals <* char
 --   Nothing -> Nothing
 
 array :: Parser JsonValue
-array = eatWhitespace >> between '[' ']' >>= (\x -> Parser $ \rest -> case parse (many (value <* char ',' <|> value)) x of
-  Just (value, _) -> Just (JsonArray value, rest)
-  Nothing -> Nothing)
+array =
+  eatWhitespace
+    >> between '[' ']'
+    >>= ( \x -> Parser $ \rest -> case parse (many (value <* char ',' <|> value)) x of
+            Just (value, _) -> Just (JsonArray value, rest)
+            Nothing -> Nothing
+        )
 
 space = char ' '
 
@@ -127,40 +131,52 @@ eatSpaces = void (many (space <|> char '\t'))
 
 eatNewlines = void (many $ char '\n')
 
-eatWhitespace = eatSpaces *> eatNewlines
+eatWhitespace = void (many (space <|> char '\t' <|> char '\n'))
 
 parseKey :: Parser String
-parseKey = seek ':' >>= (\x -> Parser $ \y -> case parse string x of
-    Just (actualKey, _) -> Just (actualKey, y)
-    Nothing -> Nothing)
+parseKey =
+  seek ':'
+    >>= ( \x -> Parser $ \y -> case parse string x of
+            Just (actualKey, _) -> Just (actualKey, y)
+            Nothing -> Nothing
+        )
+
 keyed :: Parser (String, JsonValue)
-keyed = Parser $ \x -> let
-  key = parseKey -- ok so now we have this key
-  in case parse key x of
-    Just (key, rest) ->
-      --          get rid of : and try to find a value from it
-      case parse (char ':' >> eatSpaces >> value) rest of
-      Just (value, rest') -> Just ((key, value), rest')
-      Nothing -> Nothing -- we'dprobably wanna throw an error for this
-    Nothing -> Nothing
+keyed = Parser $ \x ->
+  let key = parseKey -- ok so now we have this key
+   in case parse key x of
+        Just (key, rest) ->
+          --          get rid of : and try to find a value from it
+          case parse (char ':' >> eatWhitespace >> value) rest of
+            Just (value, rest') -> Just ((key, value), rest')
+            Nothing -> Nothing -- we'dprobably wanna throw an error for this
+        Nothing -> Nothing
 
--- this can parse the guts of an object but kind of scares me 
+-- this can parse the guts of an object but kind of scares me
 parseInternals :: Parser [(String, JsonValue)]
-parseInternals = eatSpaces >> many (optional (char ',') *> eatSpaces >> keyed <* char ',' <|> keyed)
+parseInternals = eatWhitespace >> many (optional (char ',') >> eatWhitespace >> keyed <* char ',' <|> keyed)
 
-parseFull :: Parser JsonValue
-parseFull = char '{' >> eatWhitespace >> object
 -- so we can use >> for getting things OUT of strings like
 -- (char ':' >> full) ":monkey" = monkey.
 -- what >> does is it just consumes whatever we give it and passes the rest of the string into the second function
 
 -- y = char '(' *> next <* (next <|> char ')')
 -- testing one: {\"string one\":\"value one\",\"number one\":1}
-exampleInternals="\"ppu\": 55, \"id\": \"0001\",  \"type\": \"donut\",  \"name\": \"Cake\""
-complexNested="\"id\": \"0001\",\"type\": \"donut\",\"name\": \"Cake\",\"ppu\": 55,\"batters\":{\"batter\":[{ \"id\": \"1001\", \"type\": \"Regular\" },{ \"id\": \"1002\", \"type\": \"Chocolate\" },{ \"id\": \"1003\", \"type\": \"Blueberry\" },{ \"id\": \"1004\", \"type\": \"Devil's Food\" }]},\"topping\":[{ \"id\": \"5001\", \"type\": \"None\" },{ \"id\": \"5002\", \"type\": \"Glazed\" },{ \"id\": \"5005\", \"type\": \"Sugar\" },{ \"id\": \"5007\", \"type\": \"Powdered Sugar\" },{ \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" },{ \"id\": \"5003\", \"type\": \"Chocolate\" },{ \"id\": \"5004\", \"type\": \"Maple\" }]"
+exampleInternals = "\"ppu\": 55, \"id\": \"0001\",  \"type\": \"donut\",  \"name\": \"Cake\""
+
+complexNested = "\"id\": \"0001\",\"type\": \"donut\",\"name\": \"Cake\",\"ppu\": 55,\"batters\":{\"batter\":[{ \"id\": \"1001\", \"type\": \"Regular\" },{ \"id\": \"1002\", \"type\": \"Chocolate\" },{ \"id\": \"1003\", \"type\": \"Blueberry\" },{ \"id\": \"1004\", \"type\": \"Devil's Food\" }]},\"topping\":[{ \"id\": \"5001\", \"type\": \"None\" },{ \"id\": \"5002\", \"type\": \"Glazed\" },{ \"id\": \"5005\", \"type\": \"Sugar\" },{ \"id\": \"5007\", \"type\": \"Powdered Sugar\" },{ \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" },{ \"id\": \"5003\", \"type\": \"Chocolate\" },{ \"id\": \"5004\", \"type\": \"Maple\" }]"
+
 -- simpleNested ="{\"batters\":{\"batter\":[{ \"id\": \"1001\", \"type\": \"Regular\" },{ \"id\": \"1002\", \"type\": \"Chocolate\" },{ \"id\": \"1003\", \"type\": \"Blueberry\" },{ \"id\": \"1004\", \"type\": \"Devil's Food\" }]},\"topping\":[{ \"id\": \"5001\", \"type\": \"None\" },{ \"id\": \"5002\", \"type\": \"Glazed\" },{ \"id\": \"5005\", \"type\": \"Sugar\" },{ \"id\": \"5007\", \"type\": \"Powdered Sugar\" },{ \"id\": \"5006\", \"type\": \"Chocolate with Sprinkles\" },{ \"id\": \"5003\", \"type\": \"Chocolate\" },{ \"id\": \"5004\", \"type\": \"Maple\" }]}"
 -- arrays break things
 simpleNested = "\"outside\":\"the object\",\"monkey1\" : { \"id\" : 123, \"name\": \"monkey1\", \"nested\" : {\"nested object key\": \"nested object value\"}}, \"outside\":\"the object\", \"another\": {\"object\" : \"its a string\"}"
-withAnArray = "\"outside\":\"the object\",\"array\":[1,2,3,4],\"monkey1\" : { \"id\" : 123, \"name\": \"monkey1\", \"nested\" : {\"nested object key\": \"nested object value\"}}, \"outside\":\"the object\", \"another\": {\"object\" : \"its a string\"}"
 
-(Just x) = parse parseInternals withAnArray                  
+withAnArray = "\"outside\":\"the object\",\"array\":[1,2,3,4],\"monkey1\" : { \"id\" : 123, \"name\": \"monkey1\", \"nested\" : {\"nested object key\": \"nested object value\"}}, \"outside\":\"the object\", \"another\": {\"object\" : \"its a string\"}"
+withAnArrayBraced = "{\"outside\":\"the object\",\"array\":[1,2,3,4],\"monkey1\" : { \"id\" : 123, \"name\": \"monkey1\", \"nested\" : {\"nested object key\": \"nested object value\"}}, \"outside\":\"the object\", \"another\": {\"object\" : \"its a string\"}}"
+
+parseJson :: String -> Maybe JsonValue
+parseJson x = fst <$> parse object x
+
+thingy = "\"object\" : [{ \"inside\" : \"array\"}]"
+
+
+(Just x) = parse parseInternals withAnArray
